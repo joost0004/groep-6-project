36c1399beece667b9ca0184e51c3561e4121230a
Joost <bol0004@hz.nl>
2021-05-27 14:03:12 +0200

    edit to index pages

--- laravel/resources/views/calender.blade.php
+++ laravel/resources/views/calender.blade.php
@@ -44,101 +44,101 @@
             events:'/calender',
             selectable:true,
             selectHelper: true,
-            select:function(start, end, allDay)
-            {
-                var title = prompt('Event Title:');
+            // select:function(start, end, allDay)
+            // {
+            //     var title = prompt('Event Title:');
 
-                if(title)
-                {
-                    var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
+            //     if(title)
+            //     {
+            //         var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
 
-                    var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
+            //         var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
 
-                    $.ajax({
-                        url:"/calender/action",
-                        type:"POST",
-                        data:{
-                            title: title,
-                            start: start,
-                            end: end,
-                            type: 'add'
-                        },
-                        success:function(data)
-                        {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Created Successfully");
-                        }
-                    })
-                }
-            },
-            editable:true,
-            eventResize: function(event, delta)
-            {
-                var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
-                var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
-                var title = event.title;
-                var id = event.id;
-                $.ajax({
-                    url:"/calender/action",
-                    type:"POST",
-                    data:{
-                        title: title,
-                        start: start,
-                        end: end,
-                        id: id,
-                        type: 'update'
-                    },
-                    success:function(response)
-                    {
-                        calendar.fullCalendar('refetchEvents');
-                        alert("Event Updated Successfully");
-                    }
-                })
-            },
-            eventDrop: function(event, delta)
-            {
-                var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
-                var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
-                var title = event.title;
-                var id = event.id;
-                $.ajax({
-                    url:"/calender/action",
-                    type:"POST",
-                    data:{
-                        title: title,
-                        start: start,
-                        end: end,
-                        id: id,
-                        type: 'update'
-                    },
-                    success:function(response)
-                    {
-                        calendar.fullCalendar('refetchEvents');
-                        alert("Event Updated Successfully");
-                    }
-                })
-            },
+            //         $.ajax({
+            //             url:"/calender/action",
+            //             type:"POST",
+            //             data:{
+            //                 title: title,
+            //                 start: start,
+            //                 end: end,
+            //                 type: 'add'
+            //             },
+            //             success:function(data)
+            //             {
+            //                 calendar.fullCalendar('refetchEvents');
+            //                 alert("Event Created Successfully");
+            //             }
+            //         })
+            //     }
+            // },
+            // editable:true,
+            // eventResize: function(event, delta)
+            // {
+            //     var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+            //     var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+            //     var title = event.title;
+            //     var id = event.id;
+            //     $.ajax({
+            //         url:"/calender/action",
+            //         type:"POST",
+            //         data:{
+            //             title: title,
+            //             start: start,
+            //             end: end,
+            //             id: id,
+            //             type: 'update'
+            //         },
+            //         success:function(response)
+            //         {
+            //             calendar.fullCalendar('refetchEvents');
+            //             alert("Event Updated Successfully");
+            //         }
+            //     })
+            // },
+            // eventDrop: function(event, delta)
+            // {
+            //     var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
+            //     var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
+            //     var title = event.title;
+            //     var id = event.id;
+            //     $.ajax({
+            //         url:"/calender/action",
+            //         type:"POST",
+            //         data:{
+            //             title: title,
+            //             start: start,
+            //             end: end,
+            //             id: id,
+            //             type: 'update'
+            //         },
+            //         success:function(response)
+            //         {
+            //             calendar.fullCalendar('refetchEvents');
+            //             alert("Event Updated Successfully");
+            //         }
+            //     })
+            // },
 
-            eventClick:function(event)
-            {
-                if(confirm("Are you sure you want to remove it?"))
-                {
-                    var id = event.id;
-                    $.ajax({
-                        url:"/calender/action",
-                        type:"POST",
-                        data:{
-                            id:id,
-                            type:"delete"
-                        },
-                        success:function(response)
-                        {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Deleted Successfully");
-                        }
-                    })
-                }
-            }
+            // eventClick:function(event)
+            // {
+            //     if(confirm("Are you sure you want to remove it?"))
+            //     {
+            //         var id = event.id;
+            //         $.ajax({
+            //             url:"/calender/action",
+            //             type:"POST",
+            //             data:{
+            //                 id:id,
+            //                 type:"delete"
+            //             },
+            //             success:function(response)
+            //             {
+            //                 calendar.fullCalendar('refetchEvents');
+            //                 alert("Event Deleted Successfully");
+            //             }
+            //         })
+            //     }
+            // }
         });
 
     });
