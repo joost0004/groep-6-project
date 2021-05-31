8a4be74b02a387645c5c44468bcd31c45cc28f4b
JurgenvanF <frae0004@hz.nl>
2021-05-31 14:57:46 +0200

    Changed table for admin

--- laravel/resources/views/functions/admin/admin-page/index.blade.php
+++ laravel/resources/views/functions/admin/admin-page/index.blade.php
@@ -4,15 +4,21 @@
     <table class="table">
         <thead>
         <tr>
+            <td>Functie</td>
             <td>Voornaam</td>
-            <td>E-Mail</td>
+            <td>Achternaam</td>
+            <td>Geslacht</td>
+            <td>E-mail</td>
             <td></td>
         </tr>
         </thead>
         <tbody>
         @foreach($users as $user)
             <tr>
-                <td>{{ $user->voornaam }} {{$user->achternaam}}</td>
+                <td>{{ $user->function }}</td>
+                <td>{{ $user->voornaam }}</td>
+                <td>{{ $user->achternaam }}</td>
+                <td>{{ $user->geslacht }}</td>
                 <td>{{ $user->email }}</td>
                 <td>    <form action="/admin/{{$user->id}}">
                         <button class='button is-link' type="submit">Bekijk</button>
